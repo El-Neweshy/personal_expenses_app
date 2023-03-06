b3f9459adf540b2c35b3ecd1107c690d4d88da74
Mahmoud ElNeweshy <neweshy@Mahmouds-MacBook-Pro.local>
2023-03-06 10:13:39 +0200

    add styling to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 
 void main() {
@@ -27,6 +28,7 @@ class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(id: 't1', title: 'Shoes', amount: 50, date: DateTime.now()),
     Transaction(id: 't2', title: 'Glasses', amount: 2.50, date: DateTime.now()),
+    Transaction(id: 't3', title: 'Food', amount: 5.25, date: DateTime.now()),
   ];
 
   @override
@@ -43,8 +45,8 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
           Card(
             elevation: 5,
             child: Container(
@@ -53,15 +55,64 @@ class MyHomePage extends StatelessWidget {
               child: const Text(
                 'Chart',
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 25,
                 ),
               ),
             ),
           ),
-          const Card(
-            elevation: 5,
-            child: Text('List of TX'),
-          )
+          Column(
+            children: transactions.map(
+              (tx) {
+                return Card(
+                  elevation: 5,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 3,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: const TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ).toList(),
+          ),
         ],
       ),
     );
